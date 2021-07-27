af762c73c97ba9dfecc4d0fd18ebf49f1b52b722
YaelPM <193259@ids.upchiapas.edu.mx>
2021-07-27 13:54:21 -0500

    conexión de vistas exitosa

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,8 @@
 import React from "react"
+import { io } from 'socket.io-client'
 import Box from "./box"
 import "../assets/css/game.css"
+let socket;
 
 class Game extends React.Component{
     
@@ -15,35 +17,77 @@ class Game extends React.Component{
             [2,5,8],
             [0,4,8],
             [2,4,6],
-          ];
-        console.log(array)
+          ]
 
         this.state={
+            b1:"",
+            b2:"",
+            b3:"",
+            b5:"",
+            b6:"",
+            b7:"",
+            b8:"",
+            b9:""
+        }
+        
+    }
+    componentDidMount(){
+        socket = io.connect('ws://localhost:3000')  //'ws://localhost:3000'
+        socket.on('connection',  (data) =>{
+            console.log(data)
+        } )
 
-            
+        socket.on('msj-output-client', (data) => {
+            console.log(data);
+            let posicion= data.txt
+            this.setState({
+                [posicion] : "X"
+            })
+        });
+
+    }
+    casillaLibre(button){
+        if(button.getText().equals("")){
+            return true;
+        }
+        else{
+            return false;
         }
     }
+    changeValue(e) {
+        let field = e.target.name
+        let msj= e.target.defaultValue
+
+        socket.emit('msj-input-server', { txt: field })
+        
+        this.setState({
+            [field] : "X"
+        })
+        
+    }
     
+
+    marcarCasilla1(){
+         this.setState({})
+    }
+
     render(){
         return(
-            <html>
-                <body>
-                    <h1 className="title mt-3">Tic Tac Toe</h1>
-                    <div id="tablero">
-                        <button></button>
-                        <button></button>
-                        <button></button>
-                        <button></button>
-                        <button></button>
-                        <button></button>
-                        <button></button>
-                        <button></button>
-                        <button></button>
-
-                    </div>
-                </body>
-
-            </html>
+        
+            <div className="container">
+                <h1 className="title mt-3">Tic Tac Toe</h1>
+                <div id="tablero">
+                    <input type="Button" name="b1" onClick={this.changeValue.bind(this)} defaultValue={this.state.b1}></input>
+                    <input type="Button" name="b2" onClick={this.changeValue.bind(this)} defaultValue={this.state.b2}></input>
+                    <input type="Button" name="b3" onClick={this.changeValue.bind(this)} defaultValue={this.state.b3}></input>
+                    <input type="Button" name="b4" onClick={this.changeValue.bind(this)} defaultValue={this.state.b4}></input>
+                    <input type="Button" name="b5" onClick={this.changeValue.bind(this)} defaultValue={this.state.b5}></input>
+                    <input type="Button" name="b6" onClick={this.changeValue.bind(this)} defaultValue={this.state.b6}></input>
+                    <input type="Button" name="b7" onClick={this.changeValue.bind(this)} defaultValue={this.state.b7}></input>
+                    <input type="Button" name="b8" onClick={this.changeValue.bind(this)} defaultValue={this.state.b8}></input>
+                    <input type="Button" name="b9" onClick={this.changeValue.bind(this)} defaultValue={this.state.b9}></input>
+                </div>
+            </div>
 
         )
     }
--- src/pages/app.js
+++ src/pages/app.js
@@ -1,16 +1,8 @@
 import React from 'react'
-import css from '../assets/css/app.css'
-import Game from "../components/Game";
-import Login from "../components/Login";
-import { io } from 'socket.io-client'
-class App extends React.Component {
-    componentDidMount(){
-        const socket = io.connect('ws://localhost:3000')  //'ws://localhost:3000'
-        socket.on('Conexion', (data) =>{
-            console.log(data)
-        } )
+import Game from "../components/Game"
 
-    }
+class App extends React.Component {
+    
     render(){
         return(
            <Game></Game>
