a7bab54f8ec472feab9cb0db80647da6f766b091
mayito12 <193254@ids.upchiapas.edu.mx>
2021-07-27 01:31:45 -0500

    "Conetar con el server"

--- src/pages/app.js
+++ src/pages/app.js
@@ -2,11 +2,19 @@ import React from 'react'
 import css from '../assets/css/app.css'
 import Game from "../components/Game";
 import Login from "../components/Login";
-
+import { io } from 'socket.io-client'
 class App extends React.Component {
+    componentDidMount(){
+        const socket = io.connect('ws://localhost:3000')  //'ws://localhost:3000'
+        socket.on('Conexion', (data) =>{
+            console.log(data)
+        } )
+
+    }
     render(){
         return(
-            <Login></Login>
+           <Game></Game>
+
         )
     }
 }
